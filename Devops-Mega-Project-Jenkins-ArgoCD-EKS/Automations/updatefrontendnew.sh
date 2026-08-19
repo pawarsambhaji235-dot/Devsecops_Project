f01cfbeba1ab3ad9b4e265de8c6f97ee1cf53bee
Sambhaji Pawar <pawarsambhaji235@gmail.com>
2026-08-19 21:44:33 +0530

    Update updatefrontendnew.sh

--- Devops-Mega-Project-Jenkins-ArgoCD-EKS/Automations/updatefrontendnew.sh
+++ Devops-Mega-Project-Jenkins-ArgoCD-EKS/Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-02c8505cbf4d48f8c"
+INSTANCE_ID=" i-0e7d51f2d11d0e06c"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
